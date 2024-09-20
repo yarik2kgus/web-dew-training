a00fad1d18e57e46ae1473e33d6669e1772178b7
Yaroslav <yarik22k@gmail.com>
2024-09-20 08:59:00 +0200

    20.09.2024

--- (chapter 17) Realtime-chatroom/scripts/app.js
+++ (chapter 17) Realtime-chatroom/scripts/app.js
@@ -4,9 +4,11 @@ const newChatForm = document.querySelector('.new-chat');
 
 //add a new chat
 newChatForm.addEventListener('submit', e => {
-    e.preventDefault
+    e.preventDefault();
     const message = newChatForm.message.value.trim();//Обратились к html по id
-    
+    chatroom.addChat(message) //добавляет в бд новый документ с чатом и сообщением и соответственно оно сразу появляется в чате, потому что у нас стоит лисенер на getchat()
+        .then(() => newChatForm.reset()) //очищаем строку формы 
+        .catch(err => console.log(err));
 });
 
 //class instances
