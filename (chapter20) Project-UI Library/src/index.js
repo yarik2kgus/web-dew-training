7a05b64fd83a7e9ee557b32416fd655e23545a32
yaroslav <yarik22k@gmail.com>
2024-10-09 14:46:57 +0200

    UI_Library

---
+++ (chapter20) Project-UI Library/src/index.js
@@ -0,0 +1,6 @@
+import Tooltip from "./ninja-ui/tooltip";
+
+//create a tooltip
+const tooltip = new Tooltip (document.querySelector('.tooltip'));
+
+tooltip.init();
---
+++ (chapter20) Project-UI Library/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path'); 
+
+module.exports = {  
+    entry: './src/index.js', 
+    output: {
+        path: path.resolve(__dirname, 'dist/assets'), 
+        filename: 'bundle.js' 
+    },
+    module: { 
+        rules: [
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                use: { 
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'] 
+                    }
+                }
+            },
+            {
+                test: /\.css$/, // Собирает все CSS стили и вгружает в основной файл, но он должне быть ипортриован в index.js потому что вебпак рассматривате его как "точка входа" и та мже ищет ВСЕ зависимости
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    devServer: { 
+        static: {
+            directory: path.resolve(__dirname, "dist"), 
+        },
+        devMiddleware: {
+            publicPath: '/assets/', 
+        }
+    },
+};
