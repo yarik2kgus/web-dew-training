ab6981a21a3bfc6daea3da6b678e652d31068392
yaroslav <yarik22k@gmail.com>
2024-10-14 11:39:37 +0200

    UI library end (snackbar)

--- (chapter20) Project-UI Library/src/index.js
+++ (chapter20) Project-UI Library/src/index.js
@@ -1,6 +1,7 @@
-import Tooltip from "./ninja-ui/tooltip";
+import Tooltip from "./ninja-ui/tooltip"; //импортируем все модули, которые создали
 import Dropdown from './ninja-ui/dropdown';
 import Tabs from './ninja-ui/tabs'
+import Snackbar from "./ninja-ui/snackbar";
 
 //create a tooltip
 const tooltip = new Tooltip (document.querySelector('.tooltip'));
@@ -17,3 +18,12 @@ dropdowns.forEach(dropdown => { //проходит по отданному ма�
 //create tabs
 const tabs = new Tabs(document.querySelector('.tabs'));
 tabs.init();
+
+//create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    snackbar.show('you clicked me :)');
+});
