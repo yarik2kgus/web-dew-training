4f9aee8c47550353ba84d912d6ed4cddc5e0ad72
yaroslav <yarik22k@gmail.com>
2024-10-15 13:36:03 +0200

    firebase

---
+++ (chapter21)-Using-Firebase-database-v.9/src/index.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyByQr0SKK03-holvdCQgE58VTHK1H1UedM",
+    authDomain: "fir-9-5a3e5.firebaseapp.com",
+    projectId: "fir-9-5a3e5",
+    storageBucket: "fir-9-5a3e5.appspot.com",
+    messagingSenderId: "581439187298",
+    appId: "1:581439187298:web:9758d0f7152b61ebf5e43b"
+  };
